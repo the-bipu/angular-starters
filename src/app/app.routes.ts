82242a8c4aa1f4be4d4b94871e43636d828652b8
The Bipu <127449591+the-bipu@users.noreply.github.com>
2024-12-06 03:08:01 +0530

    update home 🐦‍🔥

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,12 @@
 import { Routes } from '@angular/router';
 import { ContactComponent } from './contact/contact.component';
 import { AboutComponent } from './about/about.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { GalleryComponent } from './gallery/gallery.component';
 
 export const routes: Routes = [
-    { path: 'contact-component', component: ContactComponent },
-    { path: 'about-component', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: 'experience', component: ExperienceComponent },
+    { path: 'gallery', component: GalleryComponent },
+    { path: 'about', component: AboutComponent },
 ];
